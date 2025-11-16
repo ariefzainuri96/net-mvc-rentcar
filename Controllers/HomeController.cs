468e236727eea9b39eeec5613dd2ff309378bcba
Arief Zainuri <flaminglassoo1996@gmail.com>
2025-11-16 16:53:50 +0700

    chore: implement filtering in home index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,8 @@
-﻿using System.Diagnostics;
-using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using RentCar.Data;
 using RentCar.Models;
@@ -19,13 +20,6 @@ namespace RentCar.Controllers
             _dbContext = dbContext;
         }
 
-        public async Task<IActionResult> Index()
-        {
-            var cars = await _dbContext.Cars.ToListAsync();
-
-            return View(cars);
-        }
-
         public IActionResult Privacy()
         {
             return View();
@@ -35,6 +29,40 @@ namespace RentCar.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Index(string? brand, string? availability)
+        {
+            var query = _dbContext.Cars.AsQueryable();
+
+            if (!string.IsNullOrEmpty(brand))
+            {
+                query = query.Where(data => data.Brand.ToLower().Contains(brand));
+            }
+
+            if (string.IsNullOrEmpty(availability))
+            {
+                return View(query.Where(data => data.Status.ToLower().Equals("tersedia")).ToList());
+            }
+
+            // filter out when availability search param is not null
+
+            // we add 1 because, we want to make sure that the car is available to use
+            // by preventing using the car with exact date time of end date
+            var availabilityDateTime = DateTime
+                .ParseExact(availability, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture)
+                .AddDays(1);
+
+            var rentQuery = _dbContext.Rents.AsQueryable();
+
+            var rentedAvailableIds = rentQuery
+                .Where(data => data.Status.Equals("Active") && availabilityDateTime > data.EndDate)
+                .Select(data => data.CarId).ToList();
+
+            query =
+                query.Where(data => rentedAvailableIds.Contains(data.Id) || data.Status.ToLower().Equals("tersedia"));
+
+            return View(query.ToList());
         }
     }
 }
